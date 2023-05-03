6afc4a046602d2445f2e46b493c2e38f4e9f722c
nschmitpk <130798633+nschmitpk@users.noreply.github.com>
2023-05-02 19:46:46 -0500

    Update schm5778_lab2B_header_v001.h

--- schm5778_lab2B_header_v001.h
+++ schm5778_lab2B_header_v001.h
@@ -1,12 +1,6 @@
-/* 
- * File:   schm5778_lab2B_header_v001.h
- * Author: NoahS
- *
- * Created on February 9, 2023, 12:49 PM
- */
 
-#ifndef SCHM5778_LAB2B_HEADER_V001_H
-#define	SCHM5778_LAB2B_HEADER_V001_H
+#ifndef LAB2B_HEADER_V001_H
+#define	LAB2B_HEADER_V001_H
 
 #ifdef	__cplusplus
 extern "C" {
@@ -19,5 +13,5 @@ void write_1(void);
 }
 #endif
 
-#endif	/* SCHM5778_LAB2B_HEADER_V001_H */
+#endif
 
